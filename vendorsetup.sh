b9a4ef42e9822e5f780daee57a4510afe8938aa9
Mahesh Varma <42090586+MaheshTechnicals@users.noreply.github.com>
2024-07-28 18:00:09 +0530

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+# device/xiaomi/sm6250-common
+git clone https://github.com/SwapnilVicky/device_xiaomi_sm6250-common -b lineage-21.0 device/xiaomi/sm6250-common
+
+# vendor/xiaomi/sm6250-common
+git clone --depth=1 https://github.com/SwapnilVicky/vendor_xiaomi_sm6250-common -b lineage-21.0 vendor/xiaomi/sm6250-common
+
+# kernel/xiaomi/sm6250
+git clone --depth=1 https://github.com/SwapnilVicky/namaste_xiaomi_miatoll -b lineage-21.0 kernel/xiaomi/sm6250
+
+# hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
+
+# hardware/sony/timekeep
+git clone https://github.com/LineageOS/android_hardware_sony_timekeep -b lineage-21 hardware/sony/timekeep
