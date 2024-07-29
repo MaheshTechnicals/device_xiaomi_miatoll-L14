ab3cf8dad1a2921bebd2515d98f9c7bc571d9a89
Mahesh Varma <42090586+MaheshTechnicals@users.noreply.github.com>
2024-07-29 13:46:14 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 # device/xiaomi/sm6250-common
-git clone https://github.com/SwapnilVicky/device_xiaomi_sm6250-common -b lineage-21.0 device/xiaomi/sm6250-common
+git clone https://github.com/MaheshTechnicals/device_xiaomi_sm6250-common-L14.git -b lineage-21.0 device/xiaomi/sm6250-common
 
 # vendor/xiaomi/sm6250-common
 git clone --depth=1 https://github.com/SwapnilVicky/vendor_xiaomi_sm6250-common -b lineage-21.0 vendor/xiaomi/sm6250-common
@@ -12,3 +12,6 @@ git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 har
 
 # hardware/sony/timekeep
 git clone https://github.com/LineageOS/android_hardware_sony_timekeep -b lineage-21 hardware/sony/timekeep
+
+# vendor/xiaomi/miuicamera
+git clone https://gitlab.com/userariii/vendor-xiaomi-miuicamera.git vendor/xiaomi/miuicamera
