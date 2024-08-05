62c74a52af18e446f5eb6173b9ba36509de5dfa4
Mahesh Varma <42090586+MaheshTechnicals@users.noreply.github.com>
2024-08-05 09:58:19 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,3 +15,6 @@ git clone https://github.com/LineageOS/android_hardware_sony_timekeep -b lineage
 
 # vendor/xiaomi/miuicamera
 git clone https://github.com/clarencelol/vendor_xiaomi_miuicamera.git vendor/xiaomi/miuicamera
+
+# vendor/everest/signing
+git clone https://github.com/MaheshTechnicals/vendor_lineage-priv.git -b everest vendor/everest/signing
