5c6c849cebe36034d520cd0daf7cf779e3b15d12
Mahesh Varma <42090586+MaheshTechnicals@users.noreply.github.com>
2024-08-06 11:41:22 +0530

    Use KSU kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@ git clone https://github.com/MaheshTechnicals/device_xiaomi_sm6250-common-L14.gi
 git clone --depth=1 https://github.com/SwapnilVicky/vendor_xiaomi_sm6250-common -b lineage-21.0 vendor/xiaomi/sm6250-common
 
 # kernel/xiaomi/sm6250
-git clone --depth=1 https://github.com/SwapnilVicky/namaste_xiaomi_miatoll -b lineage-21.0 kernel/xiaomi/sm6250
+git clone --depth=1 https://github.com/SwapnilVicky/namaste_xiaomi_miatoll -b ksu kernel/xiaomi/sm6250
 
 # hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
